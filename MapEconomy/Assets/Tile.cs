6b4e03375595d0e10237ffaf104cda5bf0fcf1ac
variantc <alboy_uk@hotmail.com>
2018-11-06 22:13:07 -0500

    fist commit, trying to get height -> grey

---
+++ MapEconomy/Assets/Tile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    
+    public int x;
+    public int y;
+
+    public float height;
+    public float moisture;
+    public float vegetation;
+
+    float perlinNoiseScale = 10f;
+
+    SpriteRenderer sr;
+    public Color srColor;
+
+    private void Start()
+    {
+
+        sr = GetComponent<SpriteRenderer>();
+        if (sr == null)
+        {
+            Debug.LogError("No SpriteRenderer");
+        }
+        //else
+            //Debug.Log(sr);
+    }
+
+    public void SetHeightFromCoords(float width, float height)
+    {
+        //Debug.Log(x + y);
+        height = Mathf.PerlinNoise(x / width * perlinNoiseScale, y / height * perlinNoiseScale);
+        Debug.Log(height);
+        srColor = new Color(height, height, height, 1f);
+        this.sr.color = srColor;
+    }
+    public void SetColourFromHeight ()
+    {
+        srColor = new Color(height, height, height, 1f);
+        sr.color = srColor;
+    }
+}
---
+++ MapEconomy/Assets/World.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+    public Tile[,] tileArray;
+    public Tile tilePrefab;
+    public int mapWidth = 20;
+    public int mapHeight = 20;
+
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.transform.position = new Vector3(mapWidth / 2 - 0.5f, mapHeight / 2 - 0.5f, -10f);
+        tileArray = new Tile[mapWidth, mapHeight];
+
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < mapHeight; j++)
+            {
+                tileArray[i, j] = Instantiate(tilePrefab, this.transform);
+                tileArray[i, j].x = i;
+                tileArray[i, j].y = j;
+                tileArray[i, j].transform.position = new Vector2(i, j);
+                tileArray[i, j].transform.name = "Tile[" + i + "," + j + "]";
+            }
+        }
+
+        foreach (Tile t in tileArray)
+        {
+            t.SetHeightFromCoords(mapWidth,mapHeight);
+        }
+        //foreach (Tile t in tileArray)
+        //{
+        //    t.SetColourFromHeight();
+        //}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
